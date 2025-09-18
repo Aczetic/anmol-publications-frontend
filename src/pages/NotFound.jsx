cd48705795e72f1b125cf6ce6305fa81ce5166ad
Ananya <justdiscussanything@gmail.com>
2025-09-19 03:31:29 +0530

    fixed about-us and 404

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,7 +2,9 @@ import notFound from '../assets/not_found.jpg';
 
 const NotFound = () => {
   return (
-    <div className = 'w-full min-h-[100vh]' style = {{backgroundImage : `url(${notFound})`, backgroundSize:'cover', backgroundPosition:'50% 45%'}}>
+    <div className = 'w-full h-full overflow-hidden'>
+      <div data-aos = 'zoom-out' data-aos-delay = '100' className = 'w-full min-h-[100vh] overflow-hidden' style = {{backgroundImage : `url(${notFound})`, backgroundSize:'cover', backgroundPosition:'50% 45%'}}>
+      </div>
     </div>
   )
 }
