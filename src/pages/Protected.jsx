b59fae021b69a98346fcc3a76a53c00d57533121
Ananya <justdiscussanything@gmail.com>
2025-10-02 17:41:24 +0530

    prtected logged-in bug fix

--- src/pages/Protected.jsx
+++ src/pages/Protected.jsx
@@ -2,13 +2,12 @@ import axios from "axios";
 import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { Outlet, useNavigate } from "react-router";
-
+import { setUser } from "../features/userSlice";
 
 
 const Protected = ({children})=>{
-
+    const user = useSelector(state => state.user);
     const navigate = useNavigate();
-
     useEffect(()=>{  
 
        axios(`${import.meta.env.VITE_SERVER_URL}/auth/logged-in`, {
@@ -16,11 +15,13 @@ const Protected = ({children})=>{
        })
          .then((res) => {
            if (!res.data.success) {
+             dispatch(setUser(null));
              navigate("/login");
            }
          })
          .catch((e) => {
            if (!e.response.data.success) {
+             dispatch(setUser(null)); 
              navigate("/login");
            }
          });
