c3c98dcfcbd0196e60cdd1a338f424e9a9dcebdf
Ananya <justdiscussanything@gmail.com>
2025-10-31 19:40:25 +0530

    admin role added in dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -9,18 +9,17 @@ const Dashboard = () => {
   const timeoutRef = useRef(null);
   // to protect from unauthorized access if somebody tried doing /dashboard
   //TODO: later use redux thunk this is very ugly
-    useEffect(()=>{
+    useEffect(() => {
       clearTimeout(timeoutRef.current);
-
-      if(user && user.role !== 'principal'){
-        timeoutRef.current = setTimeout(()=> navigate('/profile'),200);
-      }else if (user && user.role == 'principal'){
-        navigate('/dashboard') // I know this is some weird logic but that user up there is getting 
-                               // null for a split second then getting actually value which is being set by app.jsx
-      }else {
-        timeoutRef.current = setTimeout(()=>navigate('/login'),200);
+      if (user && (user.role === "principal" || user.role === 'admin')) {
+        navigate("/dashboard"); // I know this is some weird logic but that user up there is getting
+        // null for a split second then getting actually value which is being set by app.jsx
+      } else if (user && user.role !== "principal" && user.role !== "admin") {
+        timeoutRef.current = setTimeout(() => navigate("/profile"), 200);
+      } else {
+        timeoutRef.current = setTimeout(() => navigate("/login"), 200);
       }
-    },[user])
+    }, [user]);
 
 
   return (
