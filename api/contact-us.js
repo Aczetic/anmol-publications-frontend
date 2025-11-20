948fd5d867a3e799dedada765cf926c634602191
Ananya <justdiscussanything@gmail.com>
2025-11-20 20:50:54 +0530

    time-update-for-testing

--- api/contact-us.js
+++ api/contact-us.js
@@ -7,7 +7,7 @@ let timeout = null;
 export default async (req,res)=>{
     if(requestCount < 1){
         if(timeout == null){
-            timeout = setTimeout(()=>{ requestCount = 0; timeout = null },5000);
+            timeout = setTimeout(()=>{ requestCount = 0; timeout = null },50000);
         }
 
         if(req.method === 'POST'){
