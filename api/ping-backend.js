3b10e2a686a95c95ecfc25d20c8d72ec769754c7
Ananya <justdiscussanything@gmail.com>
2025-11-22 12:06:34 +0530

    timeout-removed

--- api/ping-backend.js
+++ api/ping-backend.js
@@ -5,24 +5,24 @@ let timeout = null;
 const routes = ['ping', 'ping2' , 'faqs' , 'misc/testimonial' , 'books'];
 
 export default (req,res)=>{
-    if(timeout === null){
-        timeout = setTimeout(()=>{
-            const randomRoute = routes[Math.floor(Math.random()*routes.length)];
-           
-            axios.get('https://api.anmoleducationalbooks.com/'+randomRoute)
-            .then(resp=>{
+  const randomRoute = routes[Math.floor(Math.random() * routes.length)];
 
-                console.log('pinged route : ', randomRoute);
-                const randTime = Math.floor(Math.random()*15*60*1000);                    
-            
-                setTimeout(()=>{
-                    axios.get('/ping-backend')
-                }, randTime )
-                
-                res.status(200).send('success');
-            }).catch(e=>{
-                console.log(e);
-            })
-        })
-    }
+  axios
+    .get("https://api.anmoleducationalbooks.com/" + randomRoute)
+    .then((resp) => {
+      console.log("pinged route : ", randomRoute);
+      
+      const randTime = Math.floor(Math.random() * 15 * 60 * 1000);
+
+      setTimeout(() => {
+        axios.get("/ping-backend");
+      }, randTime);
+
+      res.status(200).send("success");
+   
+    })
+    
+    .catch((e) => {
+      console.log(e);
+    });
 }
